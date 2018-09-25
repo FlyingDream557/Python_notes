898e4ef497e14d64b4526038e691943ed7df08b9
FlyingDream557 <41693584+FlyingDream557@users.noreply.github.com>
2018-09-25 11:18:49 +0800

    Update use_pickle.py

--- use_pickle.py
+++ use_pickle.py
@@ -7,11 +7,13 @@ data1 = {'a': [1, 2.0, 3, 4+6j],
 
 print(type(data1))
 
+# 注意要以 ‘wb’ 的方式打开文件
 with open('s.pickle', 'wb') as f:
     pickle.dump(data1, f)
 
 print('----------------------------')
 
+# 注意要以 ‘rb’ 的方式打开文件
 with open('s.pickle', 'rb') as f1:
     pickle.load(f1)
     print(s1)
