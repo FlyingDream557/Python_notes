1dd681d38ee97a10e95d5ba6db082b12f2506c04
FlyingDream557 <41693584+FlyingDream557@users.noreply.github.com>
2018-10-08 14:18:48 +0800

    Create clockdemo2.py

---
+++ cookbook/Decorator/clockdemo2.py
@@ -0,0 +1,27 @@
+# clockdeco2.py
+
+'''
+Topic: wraps 装饰器会把相关的属性从 func 复制到 clocked 中
+'''
+
+import time
+import functools
+
+# 此为改进后的clock装饰器
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)
+        print('[%0.8fs] %s(%s) -> %r ' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
